8db6ba028871f8f676d115ce1187b5df71775671
arad <aradarvinxo@gmail.com>
2023-10-19 19:58:06 +0330

    Chore: new func for zadd

--- crypto-update/core/utils.py
+++ crypto-update/core/utils.py
@@ -17,7 +17,13 @@ def set_time(minute: int):
 
 
 
-async def get_req_data(update_time):
+def cache_data_to_redis(req_data, symbol, update_time):
+    data = str(req_data['data'][0]) if req_data.get('data') else ''
+    score = int(req_data['data'][0][0]) if req_data.get('data') else 0
+    redisClient.zadd(f"{symbol}_{update_time}min", {data:score})
+
+
+
     async with aiohttp.ClientSession() as session:
         symbols = "ETH-BTC, LTC-BTC, EOS-BTC, XRP-BTC, KCS-BTC, DIA-BTC, VET-BTC, DASH-BTC, DOT-BTC, XTZ-BTC, ZEC-BTC, BSV-BTC, ADA-BTC, ATOM-BTC, LINK-BTC, LUNA-BTC, NEO-BTC, UNI-BTC, ETC-BTC, BNB-BTC, TRX-BTC, XLM-BTC, BCH-BTC, USDC-BTC, GRT-BTC, 1INCH-BTC, AAVE-BTC, SNX-BTC, API3-BTC, CRV-BTC, MIR-BTC, SUSHI-BTC, COMP-BTC, ZIL-BTC, YFI-BTC, OMG-BTC, XMR-BTC, WAVES-BTC, MKR-BTC, COTI-BTC, SXP-BTC, THETA-BTC, ZRX-BTC, DOGE-BTC, LRC-BTC, FIL-BTC, DAO-BTC, BTT-BTC, KSM-BTC, BAT-BTC, ROSE-BTC, CAKE-BTC, CRO-BTC, XEM-BTC, MASK-BTC, FTM-BTC, IOST-BTC, ALGO-BTC, DEGO-BTC, CHR-BTC, CHZ-BTC, MANA-BTC, ENJ-BTC, IOST-BTC, ANKR-BTC, ORN-BTC, SAND-BTC, VELO-BTC, AVAX-BTC, DODO-BTC, WIN-BTC, ONE-BTC, SHIB-BTC, ICP-BTC, MATIC-BTC, CKB-BTC, SOL-BTC, VRA-BTC, DYDX-BTC, ENS-BTC, NEAR-BTC, SLP-BTC, AXS-BTC, TLM-BTC, ALICE-BTC, IOTX-BTC, QNT-BTC, SUPER-BTC, HABR-BTC, RUNE-BTC, EGLD-BTC, AR-BTC, RNDR-BTC, LTO-BTC, YGG-BTC".replace('BTC', 'USDT').split(', ')
         symbols.append("BTC-USDT")
